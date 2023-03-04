4f6e739532ff8b6f2b10aafe193d83f9430ed1ac
AbdullahRana22 <abullahr8@gmail.com>
2023-03-05 01:51:53 +0500

    first-commit

---
+++ app.js
@@ -0,0 +1,32 @@
+alert("helloworld");
+
+let rollnumber ="452"
+
+ console.log("Roll:",rollnumber);
+
+rollnumber ="499"
+console.log("Roll:",rollnumber);
+
+
+let balance = 450;
+console.log("balance: ", balance);
+
+balance = balance + 20;
+console.log("balance :",balance);
+
+balance = balance + 764;
+console.log("balance :",balance);
+
+
+
+
+
+
+
+//let balance = 420;
+
+//console.log("balance: ", balance); // 420
+
+////balance = balance + 12;
+
+//console.log("balance: ", balance); // 408
